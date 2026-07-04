79952bb1687ea8658a82ff1a58ae7d8ab68d5ddb
welo | main nixos computer <empty@empty.com>
2026-07-04 14:19:03 +0100

    fixed relay doing stuff

--- src/client.rs
+++ src/client.rs
@@ -367,7 +367,7 @@ async fn handle_udp_connect(
     // Reply to SOCKS5 client based on RNS connection result
 
     let udp_stream = UdpSocket::bind(format!("0.0.0.0:0")).await.expect("unable to get udp socket");
-    let port = udp_stream.local_addr().unwrap().port();
+    let relay_port = udp_stream.local_addr().unwrap().port();
     let relay_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
 
 
